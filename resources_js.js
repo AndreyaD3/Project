9e17fec60686769c119396738905c4724010870f
cbroadleyc <roseyauroras@gmail.com>
2023-11-04 21:03:57 -0400

    First Commit

---
+++ resources_js.js
@@ -0,0 +1,9 @@
+function displayNOAA(){
+    document.getElementById("NOAA").style.display = "block";
+}
+function displayNASA(){
+    document.getElementById("NASA").style.display = "block";
+}
+function displayOECD(){
+    document.getElementById("OECD").style.display = "block";
+}
